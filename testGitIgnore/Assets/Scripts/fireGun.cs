9601fc98c42a1d9632f4562a0424648594eca554
B533P-10\533 <talbot.nicolas.nt@gmail.com>
2018-08-27 16:56:28 -0400

    premier essaie de mouvement

---
+++ testGitIgnore/Assets/Scripts/fireGun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireGun : MonoBehaviour {
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject bulletEmitter;
+    [SerializeField] float firerate = 1;
+    // Use this for initialization
+    private float timerLastShot = 0;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerLastShot += Time.deltaTime;
+        if(timerLastShot > firerate)
+        {
+            var fire1 = Input.GetAxis("Fire1");
+            if (fire1 >0)
+            {
+                Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation);
+                timerLastShot = 0;
+            }
+	    }
+    }
+        
+}
